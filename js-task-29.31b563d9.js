15c4807445350834409571055c53fbbc0503ed94
elen-purple <anna.havrylova.2011@gmail.com>
2025-03-19 20:28:22 +0200

    Updates

--- js-task-29.31b563d9.js
+++ js-task-29.31b563d9.js
@@ -696,10 +696,10 @@ const input = document.querySelector("#time-input");
 const form = document.querySelector("#form");
 form.addEventListener("submit", (e)=>{
     e.preventDefault();
-    const value = input.value;
+    const value = Number.parseInt(input.value) * 1000;
     input.value = "";
     setTimeout(()=>{
-        console.log(`It have gone ${value}ms`);
+        console.log(`It have gone ${value / 1000}s`);
     }, Number.parseInt(value));
 });
 
